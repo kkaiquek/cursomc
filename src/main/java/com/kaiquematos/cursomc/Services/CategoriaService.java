ae30310ae60304301158b3f56eae4211b628a9e9
kkaiquek <kaiquedematoscosta@hotmail.com>
2022-06-13 17:09:36 -0300

    criando repository e service

---
+++ src/main/java/com/kaiquematos/cursomc/Services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.kaiquematos.cursomc.Services;
+
+import com.kaiquematos.cursomc.domain.Categoria;
+import com.kaiquematos.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria buscar(Integer id) {
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+
+    }
+}
