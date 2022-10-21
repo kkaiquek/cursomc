b5c3fbf46a33bef48de90ed3624507ed5bffaca2
kkaiquek <kaiquedematoscosta@hotmail.com>
2022-10-21 16:04:18 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/kaiquematos/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.kaiquematos.cursomc.repositories;
+
+import com.kaiquematos.cursomc.domain.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+}
