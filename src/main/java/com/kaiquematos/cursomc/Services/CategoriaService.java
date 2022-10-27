d13e9349a77d07406022131691333608354e620b
kkaiquek <kaiquedematoscosta@hotmail.com>
2022-10-27 17:39:31 -0300

    Ajustes no endpoint /categorias /{id}

--- src/main/java/com/kaiquematos/cursomc/Services/CategoriaService.java
+++ src/main/java/com/kaiquematos/cursomc/Services/CategoriaService.java
@@ -2,9 +2,11 @@ package com.kaiquematos.cursomc.Services;
 
 import com.kaiquematos.cursomc.domain.Categoria;
 import com.kaiquematos.cursomc.repositories.CategoriaRepository;
+import com.kaiquematos.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.Id;
 import java.util.Optional;
 
 @Service
@@ -15,6 +17,11 @@ public class CategoriaService {
 
     public Categoria buscar(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
+        if (obj == null) {
+            throw new ObjectNotFoundException("Objeto não encontrado! Id" + id
+            + ", Tipo: " + Categoria.class.getName());
+
+        }
         return obj.orElse(null);
     }
 }
